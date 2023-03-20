881c5469091c4e4c70249c4613f8e4dfa1b31292
Wellington Santos <wellington.santos@medprevonline.com>
2023-03-20 13:51:23 -0300

    corrigindoo om app js

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodadno o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("Commit feito fora da aula")
+console.log("aula adicionando arquivos")
