2c37bcc66f0a9e1ec70c34582f1fe15de1b7246f
Wellington Santos <wellington.santos@medprevonline.com>
2023-03-20 11:03:17 -0300

    voltando para o status fora da aula

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodadno o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("Commit feito fora da aula")
-console.log("Enviando para o GitHub")
