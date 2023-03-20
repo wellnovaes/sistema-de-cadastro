c0f8c588e205dbd888cfbe1dff42c87412821928
wellmedprev <125909189+wellmedprev@users.noreply.github.com>
2023-03-20 10:45:26 -0300

    commit feito fora da aula

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodadno o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("Commit feito fora da aula")
